11f9bd762b58698cb506a1ca29401347641dca52
Anon Dev <michaelerastus9@gmail.com>
2025-05-28 10:18:04 +0100

    updated route

--- routes/web.php
+++ routes/web.php
@@ -4,21 +4,4 @@ use App\Livewire\MenuView;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/test-remote-wp', function () {
-    try {
-        $posts = DB::connection('wordpress')->table('posts')
-            ->where('post_type', 'mp_menu_item')
-            ->where('post_status', 'publish')
-            ->get();
-
-        return response()->json($posts);
-    } catch (\Exception $e) {
-        return response()->json(['error' => $e->getMessage()]);
-    }
-});
-
-Route::get('/menu', MenuView::class)->name('menu')->lazy();
+Route::get('/', MenuView::class)->name('menu')->lazy();
